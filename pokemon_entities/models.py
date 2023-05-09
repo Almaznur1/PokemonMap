9abf5d75c07cf62ad3a6b675438116615aabec98
AlmazNuriakhmetov <aboohnifa@gmail.com>
2023-05-09 11:47:53 +0300

    add english and japan pokemon names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
     image = models.ImageField(null=True, blank=True)
     description = models.TextField(blank=True)
 
