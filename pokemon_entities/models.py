103329c55e330525ab3fbb01987e220de5bbda9b
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-05-09 19:42:50 +0300

    reduce database content requirements

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,13 +1,12 @@
-from django.db import models  # noqa F401
-from datetime import datetime
+from django.db import models
 
 
 class Pokemon(models.Model):
     title = models.CharField('Название', max_length=200)
-    title_en = models.CharField('Название на английском', max_length=200, blank=True)
-    title_jp = models.CharField('Название на японском', max_length=200, blank=True)
+    title_en = models.CharField('Название на английском', max_length=200, null=True, blank=True)
+    title_jp = models.CharField('Название на японском', max_length=200, null=True, blank=True)
     image = models.ImageField('Картинка', null=True, blank=True)
-    description = models.TextField('Описание', blank=True)
+    description = models.TextField('Описание', null=True, blank=True)
     previous_evolution = models.ForeignKey('self',
                                            verbose_name='Из кого эволюционировал',
                                            on_delete=models.SET_NULL,
@@ -19,12 +18,12 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    lat = models.FloatField('', null=True, blank=True)
-    lon = models.FloatField('', null=True, blank=True)
+    lat = models.FloatField('Широта', null=True, blank=True)
+    lon = models.FloatField('Долгота', null=True, blank=True)
     pokemon = models.ForeignKey(Pokemon, related_name='pokemon_entities',
                                 on_delete=models.CASCADE)
-    appeared_at = models.DateTimeField('Время появления')
-    disappeared_at = models.DateTimeField('Время исчезновения')
+    appeared_at = models.DateTimeField('Время появления', null=True, blank=True)
+    disappeared_at = models.DateTimeField('Время исчезновения', null=True, blank=True)
     level = models.IntegerField('Уровень', null=True, blank=True)
     health = models.IntegerField('Здоровье', null=True, blank=True)
     attack = models.IntegerField('Атака', null=True, blank=True)
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,7 +1,5 @@
 import folium
-import json
 
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from .models import PokemonEntity, Pokemon
 from django.utils.timezone import localtime
@@ -29,9 +27,6 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    # with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-    #     pokemons = json.load(database)['pokemons']
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     pokemon_entities = PokemonEntity.objects.filter(
         appeared_at__lt=localtime(), disappeared_at__gt=localtime()
@@ -59,18 +54,8 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    # with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-    #     pokemons = json.load(database)['pokemons']
-
     requested_pokemon = Pokemon.objects.get(id=int(pokemon_id))
     requested_pokemon_entities = requested_pokemon.pokemon_entities.all()
-
-    # for pokemon in pokemons:
-    #     if pokemon['pokemon_id'] == int(pokemon_id):
-    #         requested_pokemon = pokemon
-    #         break
-    # else:
-    #     return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in requested_pokemon_entities:
