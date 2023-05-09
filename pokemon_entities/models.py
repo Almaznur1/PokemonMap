33f22d25726ec4a78ae428f901ef0abfff702be5
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-05-09 19:25:30 +0300

    add verbose name to fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,12 +3,13 @@ from datetime import datetime
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField(blank=True)
+    title = models.CharField('Название', max_length=200)
+    title_en = models.CharField('Название на английском', max_length=200, blank=True)
+    title_jp = models.CharField('Название на японском', max_length=200, blank=True)
+    image = models.ImageField('Картинка', null=True, blank=True)
+    description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey('self',
+                                           verbose_name='Из кого эволюционировал',
                                            on_delete=models.SET_NULL,
                                            null=True, blank=True,
                                            related_name='next_evolution')
@@ -18,14 +19,14 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    lat = models.FloatField(null=True, blank=True)
-    lon = models.FloatField(null=True, blank=True)
+    lat = models.FloatField('', null=True, blank=True)
+    lon = models.FloatField('', null=True, blank=True)
     pokemon = models.ForeignKey(Pokemon, related_name='pokemon_entities',
                                 on_delete=models.CASCADE)
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    attack = models.IntegerField(null=True, blank=True)
-    defense = models.IntegerField(null=True, blank=True)
-    stamina = models.IntegerField(null=True, blank=True)
+    appeared_at = models.DateTimeField('Время появления')
+    disappeared_at = models.DateTimeField('Время исчезновения')
+    level = models.IntegerField('Уровень', null=True, blank=True)
+    health = models.IntegerField('Здоровье', null=True, blank=True)
+    attack = models.IntegerField('Атака', null=True, blank=True)
+    defense = models.IntegerField('Защита', null=True, blank=True)
+    stamina = models.IntegerField('Выносливость', null=True, blank=True)
